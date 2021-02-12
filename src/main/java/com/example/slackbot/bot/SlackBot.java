dae96a833ac4b282587f37c67e66cdb84b569c08
Lorenzo Rodriguez <Lorenzo1906@gmail.com>
2021-02-11 22:51:32 -0600

    Initial commit

---
+++ src/main/java/com/example/slackbot/bot/SlackBot.java
@@ -0,0 +1,36 @@
+package com.example.slackbot.bot;
+
+import me.ramswaroop.jbot.core.slack.Bot;
+import me.ramswaroop.jbot.core.slack.Controller;
+import me.ramswaroop.jbot.core.slack.EventType;
+import me.ramswaroop.jbot.core.slack.models.Event;
+import me.ramswaroop.jbot.core.slack.models.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.socket.WebSocketSession;
+
+@Profile("slack")
+public class SlackBot extends Bot {
+
+    private static final Logger logger = LoggerFactory.getLogger(SlackBot.class);
+
+    @Value("${slackBotToken}")
+    private String slackToken;
+
+    @Override
+    public String getSlackToken() {
+        return slackToken;
+    }
+
+    @Override
+    public Bot getSlackBot() {
+        return this;
+    }
+
+    @Controller(events = {EventType.DIRECT_MENTION, EventType.DIRECT_MESSAGE})
+    public void onReceiveDM(WebSocketSession session, Event event) {
+        reply(session, event, new Message("Hi, I am " + slackService.getCurrentUser().getName()));
+    }
+}
