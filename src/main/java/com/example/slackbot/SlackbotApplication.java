1221d5dc1917532041b13b751c36f08bc057e20d
Lorenzo Rodriguez <Lorenzo1906@gmail.com>
2021-08-18 22:17:42 -0600

    Removed Teams configuration

--- src/main/java/com/example/slackbot/SlackbotApplication.java
+++ src/main/java/com/example/slackbot/SlackbotApplication.java
@@ -1,32 +1,12 @@
 package com.example.slackbot;
 
-import com.example.slackbot.bot.EchoBot;
-import com.microsoft.bot.builder.Bot;
-import com.microsoft.bot.integration.AdapterWithErrorHandler;
-import com.microsoft.bot.integration.BotFrameworkHttpAdapter;
-import com.microsoft.bot.integration.Configuration;
-import com.microsoft.bot.integration.spring.BotController;
-import com.microsoft.bot.integration.spring.BotDependencyConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({BotController.class})
-public class SlackbotApplication extends BotDependencyConfiguration {
+public class SlackbotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SlackbotApplication.class, args);
-	}
-
-	@Bean
-	public Bot getBot() {
-		return new EchoBot();
-	}
-
-	@Override
-	public BotFrameworkHttpAdapter getBotFrameworkHttpAdaptor(Configuration configuration) {
-		return new AdapterWithErrorHandler(configuration);
 	}
 }
